3aa265db58d7a81d075558b8d5d37449fa5998f8
paresh solanki <bandralites@gmail.com>
2015-08-24 14:13:19 -0400

    read

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var db = new sqlite3.Database('forums.db');
 app.use(express.static('public'));
  
 
-//middleware
+
 var bodyParser = require('body-parser');
 var urlencodedBodyParser = bodyParser.urlencoded({extended: false});
 app.use(urlencodedBodyParser);
@@ -16,19 +16,20 @@ app.use(methodOverride('_method'));
 var addon =1;
 
 
-//config
+
 app.listen(3000, function() {
   console.log("I'm listening!");
 });
 
 app.get("/", function(req, res){
      db.all("SELECT * FROM topics", function(err, rows){
-     	// console.log(rows);
-	 	var indexHtml = fs.readFileSync("./index.html", "utf8");
-     	var renderedIndex= ejs.render(indexHtml, {rows:rows})
-     	res.send(renderedIndex);
 
+			var indexHtml = fs.readFileSync("./index.html", "utf8");
+	     	var renderedIndex= ejs.render(indexHtml, {rows:rows})
+	     	res.send(renderedIndex);
+     	
 	});
+
 });
 
 app.get("/newTopic", function(req, res){
@@ -39,7 +40,6 @@ app.get("/newTopic", function(req, res){
 
 
 app.post("/yourTopic", function(req, res){
-	// console.log(req.body);
 	var username = req.body.username;
 	var title = req.body.title;
 	var post = req.body.post;
@@ -51,17 +51,13 @@ app.post("/yourTopic", function(req, res){
 
 
 app.get("/yourTopic/:id", function(req, res){
-   // console.log(req.params.id);
 	var html = fs.readFileSync("./yourTopic.html", "utf8");
 	   	db.get("SELECT * FROM topics WHERE id=?", req.params.id , function(error,row){
-	   		// console.log(row);
-     			db.all("SELECT * FROM  comments WHERE topics_id = ?", req.params.id ,function(err, rows){
-				   console.log(rows);
-					 db.all("SELECT * FROM likes where like_topic_id =?", req.params.id, function(err,rowss){
-
-					// console.log(err);
-					var rendered = ejs.render(html, { comments: rows, topic: row, likes:rowss });
-					res.send(rendered);
+ 			db.all("SELECT * FROM  comments WHERE topics_id = ?", req.params.id ,function(err, rows){
+			   console.log(rows);
+				 db.all("SELECT * FROM likes where like_topic_id =?", req.params.id, function(err,rowss){
+						var rendered = ejs.render(html, { comments: rows, topic: row, likes:rowss });
+						res.send(rendered);
 					 })
      		})
 	});
---
+++ readme.md
@@ -0,0 +1,18 @@
+Read Me
+ 
+ Project Forum 
+
+ M.V.P
+ * A Regular daily forum
+ * Where a User can post a Topic and material
+ * Visitors can view Topics and the comments on the topics and like them
+ * Visitors  can comment on the Topics
+
+Format 
+* The forum is a Web Application which has 3 pages
+   Index Page 
+   Topic Page and 
+   New Topic Page
+* Javascript was used to program the page, SQLite3 was used to persist the data, and HTML and CSS was to style the individual pages.
+
+  
